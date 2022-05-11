77dbf41433e540f5852e5a531013f053c65269e0
Justin <justinrkinsman@gmail.com>
2022-05-11 15:48:09 -0300

    Add operators buttons to display

--- javascript.js
+++ javascript.js
@@ -50,35 +50,44 @@ function operate(operand1, operator, operand2){
 let operand = document.querySelectorAll('button')
 for (const button of operand) {
     button.addEventListener('click', () => {
-    if (button.textContent != 'Clear') {
+    if ((button.textContent == '1') || (button.textContent == '2') || (button.textContent == '3') || 
+        (button.textContent == '4') || (button.textContent == '5') || (button.textContent == '6') || 
+        (button.textContent == '7') || (button.textContent == '8') || (button.textContent == '9')){
         modifyDisplayLarge(button.textContent)
-    }else{
-        modifyDisplayLarge('') && modifyDisplayMini('')
-    }
-})}
-
-let operators = document.querySelectorAll('#operatorBtns')
-for (const button of operators) {
-    button.addEventListener('click', () => {
-    if ((button.textContent === '+') || (button.textContent === '-') || (button.textContent === '*') || 
-        (button.textContent === '/')){ 
-        modifyDisplayMini(button.textContent)
-    }else{
-        modifyDisplayMini('')
+    }else if ((button.textContent == '+') || (button.textContent == '-') || (button.textContent == 'x') || 
+                (button.textContent == '/')){
+        modifyDisplayMini(`${displayLarge.textContent} ${button.textContent}`)
+    }else if (button.textContent == 'Clear'){
+        modifyDisplayLarge('0'), modifyDisplayMini('')
     }
 })}
 
 function modifyDisplayLarge(input) {
     const displayLarge = document.querySelector('#displayLarge')
     displayLarge.textContent = input
-    return(input)
 }
 
 function modifyDisplayMini(input) {
     const displayMini = document.querySelector('#displayMini')
     displayMini.textContent = input
-    return(input)
 }
-/*let operator = button.textContent
+
+
+
+
+
+
+/*let operators = document.querySelectorAll('#operatorBtns')
+for (const button of operators) {
+    button.addEventListener('click', () => {
+    if (!(button.textContent === '+') || (button.textContent === '-') || (button.textContent === '*') || 
+        (button.textContent === '/')){ 
+        console.log(button.textContent)
+    }else{
+        console.log('Hello')
+    }
+})}
+
+let operator = button.textContent
     let operand1 = button.textContent
     let operand2 = button.textContent*/
